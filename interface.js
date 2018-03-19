1a1d87b43847d149365516cabddb6340ee427afe
markthesis <36646917+markthesis@users.noreply.github.com>
2018-03-19 13:48:57 -0400

    Add files via upload

---
+++ interface.js
@@ -0,0 +1,133 @@
+function intro(tempX, tempY) {
+  push();
+
+  translate(tempX, tempY);
+  background(0, 0, 0);
+  noStroke();
+  //subtitle
+  push();
+  fill(0, 0, 100);
+  textSize(8);
+  text("U S E    I N    F U L L S C R E E N    F O R    B E S T    R E S U L T S", 0, height/-4);
+  text("D E M O    V E R S I O N    2 . 2", 0, height/-4 - 12);
+  translate(0, height/4);
+  rectMode(CENTER);
+  rect(0, 0, 275, 30, 30);
+
+  fill(0, 0, 0);
+  textSize(10);
+  text("H I T   T H E   S P A C E B A R   T O   B E G I N", -10, 4);
+  arrow(124 + 1.2*sin(6*frameCount), 0, 5);
+  pop();
+
+  push();
+  scale(height/900);
+  fill(0, 0, 100);
+  translate(-6.5*tracking, 22*size);
+
+  var waveHeight = 0.75*size;
+  var waveWeight = 0.8*size;
+  var waveSize = 3*size;
+  var speed = 2;
+  var weightChange = 0.7;
+  var subtracking = tracking*1.02;
+  var subwaveWeight = 0.7*size;
+
+  P(0, 0, waveWeight - weightChange*cos(speed*frameCount), sin(speed*frameCount)*waveHeight+waveSize);
+  H(2*tracking, 0, waveWeight - weightChange*cos(speed*frameCount-30), sin(speed*frameCount-30)*waveHeight+waveSize);
+  O(4*tracking, 0, waveWeight - weightChange*cos(speed*frameCount-60), sin(speed*frameCount-60)*waveHeight+waveSize);
+  N(6*tracking, 0, waveWeight - weightChange*cos(speed*frameCount-90), sin(speed*frameCount-90)*waveHeight+waveSize);
+  E(8*tracking, 0, waveWeight - weightChange*cos(speed*frameCount-120), sin(speed*frameCount-120)*waveHeight+waveSize);
+  T(10*tracking, 0, waveWeight - weightChange*cos(speed*frameCount-150), sin(speed*frameCount-150)*waveHeight+waveSize);
+  I(11.5*tracking, 0, waveWeight - weightChange*cos(speed*frameCount-180), sin(speed*frameCount-180)*waveHeight+waveSize);
+  C(13*tracking, 0, waveWeight - weightChange*cos(speed*frameCount-210), sin(speed*frameCount-210)*waveHeight+waveSize);
+
+  scale(0.73);
+  translate(-0.3*tracking, 115);
+
+  T(0, 0, subwaveWeight, 0.05*size);
+  Y(2*subtracking, 0, subwaveWeight, 0.05*size);
+  P(4*subtracking, 0, subwaveWeight, 0.05*size);
+  O(6*subtracking, 0, subwaveWeight, 0.05*size);
+  G(8*subtracking, 0, subwaveWeight, 0.05*size);
+  R(10*subtracking, 0, subwaveWeight, 0.05*size);
+  A(12*subtracking, 0, subwaveWeight, 0.05*size);
+  P(14*subtracking, 0, subwaveWeight, 0.05*size);
+  H(16*subtracking, 0, subwaveWeight, 0.05*size);
+  Y(18*subtracking, 0, subwaveWeight, 0.05*size);
+  pop();
+
+  pop();
+}
+
+function arrow(tempX, tempY, tempSize) {
+  push();
+  translate(tempX, tempY);
+  triangle(0, 0, -2*tempSize, tempSize, -2*tempSize, -1*tempSize);
+  pop();
+}
+
+function instructions() {
+  push();
+  translate(width/2, height/2);
+  fill(0, 0, 100);
+  noStroke();
+
+  if (state == 1) {
+    textSize(10);
+    text("B E G I N    S P E E C H    T O    T Y P E    C O N V E R S I O N", 0, height/-4);
+    textSize(15);
+    text("Hold down the mouse while speaking to record your voice.", 0, height/4);
+    textSize(8);
+    text("I F    N O T H I N G    H A P P E N S ,    P R E S S    ' 0 '    A N D    T R Y    A G A I N", 0, height/4 + height/40);
+    push();
+    noFill();
+    stroke(0, 0, 100);
+    rect(0, -height/100, width/2.5, height/2.5, height/50);
+    pop();
+    volFeedback(0, 0);
+  } else if (state == 2) {
+    textSize(10);
+    text("S P E E C H    P R O C E S S E D", 0, height/-4);
+    textSize(15);
+    text("Hit the spacebar to generate your phonetic typography!", 0, height/4);
+    push();
+    noFill();
+    stroke(0, 0, 100);
+    rect(0, -height/100, width/2.5, height/2.5, height/50);
+    pop();
+  } else if (state == 3) {
+    push();
+    translate(0, height/4);
+    rectMode(CENTER);
+    rect(0, 0, 295, 30, 30);
+
+    fill(0, 0, 0);
+    textSize(10);
+    text("H I T   T H E   S P A C E B A R   T O   R E S T A R T", -12, 4);
+    arrow(130 + 1.2*sin(6*frameCount), 0, 5);
+    pop();
+  }
+
+  pop();
+}
+
+function volFeedback(x, y) {
+  push();
+  translate(x, y);
+  var tempS = 2;
+  translate(5*tempS * (volHistory.length-1), 0);
+
+  if (mouseIsPressed) {
+    fill(0, 0, 100);
+    for (var i = 0; i < volHistory.length; i++) {
+      rect(i * -10*tempS, 0, 4*tempS, 13*tempS + volHistory[i]*3*tempS, 4*tempS);
+    }
+  } else {
+    fill(0, 0, 20);
+    for (var i = 0; i < volHistory.length; i++) {
+      ellipse(i * -10*tempS, 0, 4*tempS, 4*tempS);
+    }
+  }
+  pop();
+}
---
+++ sketch.js
@@ -0,0 +1,244 @@
+/*
+created by Mark Chan (@markcwy // markcwy.com)
+if using any part of this code for your own work,
+please don't forget to give credit! Thanks!!
+*/
+
+//text inputs
+var sentence, size, tracking;
+var xPos, yPos, xMargin, xBreak;
+//sound inputs
+var talk, mic, freq;
+var minF, maxF, minV, maxV;
+var freqArray, volArray;
+var avgFreqArray, avgVolArray;
+var volHistory;
+//switches
+var genSwitch, recSwitch;
+//misc
+var state;
+var HKNova;
+
+function preload() {
+  HKNova = loadFont('assets/HKNovaSB.otf');
+}
+
+function refresh() {
+  genSwitch = false;
+  recSwitch = false;
+  volArray = [];
+  freqArray = [];
+  volHistory = [];
+  for (var i = 0; i < 16; i++) {
+    volHistory[i] = 0;
+  }
+  state = 0;
+  size = 3;
+  tracking = 12*size;
+}
+
+function speechRefresh() {
+  talk = new p5.SpeechRec();
+  talk.start();
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB, 360, 100, 100, 1);
+  angleMode(DEGREES);
+  rectMode(CENTER)
+  textAlign(CENTER);
+  textFont(HKNova);
+  mic = new p5.AudioIn();
+  mic.start();
+  freq = new p5.FFT(0.2, 16);
+  freq.setInput(mic);
+  refresh();
+  frameRate(60);
+}
+
+function draw() {
+  push();
+
+  if (state == 0) {
+    intro(width/2, height/2);
+  } else {
+
+    if (recSwitch) {
+      background(0, 0, 0);
+      textStyling();
+      var spectrum = freq.analyze(16);
+      var f10 = map(spectrum[9], 0, 255, 0, 25);
+      var f11 = map(spectrum[10], 0, 255, 0, 25);
+      var vol = map(mic.getLevel()*1000, 0, 400, 0, 50);
+      var fOutput = map(f10 + f11, 0, 50, minF, maxF);
+      var volOutput = map(vol, 0, 50, minV, maxV);
+
+      if (talk.resultValue==true) {
+        sentence = talk.resultString;
+        console.log(sentence);
+        if (state == 1) {
+          state++;
+        }
+      }
+
+      if (mouseIsPressed){
+        if (fOutput > 0 && volOutput > 0) {
+          freqArray.push(constrain(fOutput, maxF, minF));
+          volArray.push(constrain(volOutput, minV, maxV));
+          console.log(freqArray);
+          console.log(volArray);
+        }
+
+        //display the little sound wave thing
+        if (!genSwitch) {
+          volHistory.push(constrain(volOutput, minV, maxV));
+          if (volHistory.length > 15) {
+            volHistory.splice(0, 1);
+          }
+        }
+
+      }
+    }
+
+    if (genSwitch){
+      avgFreqValues();
+      avgVolValues();
+      for (var i = 0; i < sentence.length; i++) {
+        variableType(sentence.substring(i, i + 1), avgFreqArray[i], avgVolArray[i]);
+        if (xPos > (4/5)*windowWidth + 2.5*tracking) {
+          xBreak = xPos;
+          yPos += 65*size;
+          xPos = 0;
+        }
+      }
+    }
+  }
+  pop();
+
+  instructions();
+  textSize(8);
+  fill(0, 0, 100);
+  text("I F    S T U C K ,    R E F R E S H    T H E    P A G E    ( C M D + R )", width/2, height - 30);
+
+}
+
+function keyPressed() {
+  if (key == ' ') {
+    if (state >= 0 && state < 4) {
+      state ++;
+    } else {
+      state = 0;
+    }
+
+    if (state == 1) {
+      speechRefresh();
+      recSwitch = !recSwitch;
+    } else if (state == 3) {
+      genSwitch = !genSwitch;
+    } else if (state == 4) {
+      refresh();
+      speechRefresh();
+    }
+
+  } else if (key == '0') {
+    volArray = [];
+    freqArray = [];
+    speechRefresh();
+  }
+  return false;
+}
+
+function textStyling() {
+
+  //if current x position is greater than the longest line, use xPos value
+  if (xPos >= xBreak) {
+    translate(width/2 - (xPos/2), windowHeight/2 + 35*size - ((3/4)*yPos));
+  } else {
+    translate(width/2 - (xBreak/2), windowHeight/2 + 35*size - ((3/4)*yPos));
+  }
+
+  //resets all code to 0 at start of draw cycle
+  xPos = 0;
+  yPos = 0;
+  xBreak = 0;
+  minF = size;
+  maxF = -2*size;
+  minV = -1*size;
+  maxV = 5*size;
+
+  fill(0, 0, 100);
+  noStroke();
+
+}
+
+function avgFreqValues() {
+  startPos = 0;
+  //find out how many values of frequency are mapped to a single letter
+  subSet = round(freqArray.length/sentence.length);
+
+  //creates a new array with at sentence.length with 0s;
+  avgFreqArray = zeroArray();
+
+  // for every element of avgFreqArray
+  for (var j = 0; j < sentence.length; j++) {
+
+    //test to see if adding a subSet will cause it to overshoot freqArray.length
+    var x = startPos + subSet;
+    if (x < freqArray.length) {
+
+      //for every k elements in freqArray
+      for (var k = startPos; k < (subSet*(j + 1)); k ++) {
+        avgFreqArray[j] += (freqArray[k]/subSet);
+      }
+
+      //skip to the next subSet of freqArray
+      startPos += subSet;
+
+    }
+    else {
+      //for the remainder of the array that doesn't = subSet
+      for (var k = startPos; k < freqArray.length; k++) {
+        avgFreqArray[j] += (freqArray[k])/(freqArray.length - startPos);
+      }
+    }
+  }
+
+  console.log("freq:" + avgFreqArray);
+}
+
+function avgVolValues() {
+  startPos = 0;
+  subSet = round(volArray.length/sentence.length);
+
+  avgVolArray = zeroArray();
+
+  for (var j = 0; j < sentence.length; j++) {
+
+    var x = startPos + subSet;
+    if (x < volArray.length) {
+
+      for (var k = startPos; k < (subSet*(j + 1)); k ++) {
+        avgVolArray[j] += (volArray[k]/subSet);
+      }
+      startPos += subSet;
+
+    } else {
+
+      for (var k = startPos; k < volArray.length; k++) {
+        avgVolArray[j] += (volArray[k])/(volArray.length - startPos);
+
+      }
+    }
+  }
+  console.log("vol:" + avgVolArray);
+}
+
+function zeroArray() {
+  //creates array of zeros with sentence.length;
+  emptyArray = new Array(sentence.length);
+  for (var i = 0; i < sentence.length; i ++) {
+    emptyArray[i] = 0;
+  }
+  return emptyArray;
+}
