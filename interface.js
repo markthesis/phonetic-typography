7f60b0ed2d245098bd131f681e676418840d2c36
markthesis <36646917+markthesis@users.noreply.github.com>
2018-03-26 18:29:53 -0400

    Update interface.js

--- interface.js
+++ interface.js
@@ -8,15 +8,16 @@ function intro(tempX, tempY) {
   push();
   fill(0, 0, 100);
   textSize(8);
-  text("U S E    I N    F U L L S C R E E N    F O R    B E S T    R E S U L T S", 0, height/-4);
-  text("D E M O    V E R S I O N    2 . 3 . 1", 0, height/-4 - 12);
+  text("A N D   I N   F U L L S C R E E N    F O R    B E S T    R E S U L T S", 0, height/-4);
+  text("U S E    I N    A   Q U I E T   L O C A T I O N", 0, height/-4 - 15);
+  text("D E M O    V E R S I O N    3 . 0", 0, height/-4 - 30);
   translate(0, height/4);
-  rect(0, 0, 275, 30, 30);
+  rect(0, 0, 180, 30, 30);
 
   fill(0, 0, 0);
   textSize(10);
-  text("H I T   T H E   S P A C E B A R   T O   B E G I N", -10, 4);
-  arrow(124 + 1.2*sin(6*frameCount), 0, 5);
+  text("C L I C K   T O   B E G I N", -10, 4);
+  arrow(75 + 1.2*sin(6*frameCount), 0, 5);
   pop();
 
   push();
@@ -84,37 +85,62 @@ function instructions() {
   noStroke();
 
   if (state == 1) {
+    background(0, 0, 0);
+    textSize(10);
+    text("L E T T I N G   W O R D S", 0, height/-4);
+    text("S P E A K   F O R   T H E M S E L V E S", 0, height/-4 + 15);
+    textSize(15);
+    text("This project aims to give typography\nthe ability to convey tonality of speech.", 0, -100)
+    text("A lot of meaning is lost when we type out\nwhat we're saying into letters on a screen\nbecause these characters fail to convey\nhow we are saying them.", 0, -40);
+    text("This program takes both what you say\nand how you say it, and translates that\ninto unique letterforms, allowing you to\nsee how a sentence is being said.", 0, 60);
+    push();
+    stroke(0, 0, 100);
+    line(-50, height/-4 + 50, 50, height/-4 + 50);
+    translate(0, height/4);
+    noStroke();
+    rect(0, 0, 205, 30, 30);
+    fill(0, 0, 0);
+    textSize(10);
+    text("C L I C K   T O   C O N T I N U E", -11, 4);
+    arrow(88 + 1.2*sin(6*frameCount), 0, 5);
+    pop();
+  } else if (state == 2) {
     textSize(10);
     text("B E G I N    S P E E C H    T O    T Y P E    C O N V E R S I O N", 0, height/-4);
     textSize(15);
-    text("Hold down the mouse while speaking to record your voice.", 0, height/4);
-    textSize(8);
-    text("I F    N O T H I N G    H A P P E N S ,    P R E S S    ' 0 '    A N D    T R Y    A G A I N", 0, height/4 + height/40);
+    text("Hit the spacebar to start and stop recording.", 0, height/4);
     push();
     noFill();
     stroke(0, 0, 100);
     rect(0, -height/100, 560 - 1400*30/width, height/2.5, height/50);
     pop();
     volFeedback(0, 0);
-  } else if (state == 2) {
+  } else if (state == 3) {
     textSize(10);
     text("S P E E C H    P R O C E S S E D", 0, height/-4);
     textSize(15);
-    text("Hit the spacebar to generate your phonetic typography!", 0, height/4);
     push();
     noFill();
     stroke(0, 0, 100);
     rect(0, -height/100, 560 - 1400*30/width, height/2.5, height/50);
     checkmark(0, 0);
-    pop();
-  } else if (state == 3) {
-    push();
     translate(0, height/4);
-    rect(0, 0, 295, 30, 30);
+    noStroke();
+    fill(0, 0, 100);
+    rect(0, 0, 205, 30, 30);
     fill(0, 0, 0);
     textSize(10);
-    text("H I T   T H E   S P A C E B A R   T O   R E S T A R T", -12, 4);
-    arrow(130 + 1.2*sin(6*frameCount), 0, 5);
+    text("C L I C K   T O   C O N T I N U E", -11, 4);
+    arrow(88 + 1.2*sin(6*frameCount), 0, 5);
+    pop();
+  } else if (state == 4) {
+    push();
+    translate(0, height/4);
+    rect(0, 0, 195, 30, 30);
+    fill(0, 0, 0);
+    textSize(10);
+    text("C L I C K   T O   R E S T A R T", -12, 4);
+    arrow(83 + 1.2*sin(6*frameCount), 0, 5);
     pop();
   }
 
@@ -127,7 +153,9 @@ function volFeedback(x, y) {
   var tempS = 2;
   translate(5*tempS * (volHistory.length-1), 0);
 
-  if (mouseIsPressed) {
+  if (startRecSwitch) {
+    fill(0, 100, 100, abs(sin(frameCount*3)));
+    ellipse(102 - 700*30/width, -height/5 + 20, 20, 20);
     fill(0, 0, 100);
     for (var i = 0; i < volHistory.length; i++) {
       rect(i * -10*tempS, 0, 4*tempS, 13*tempS + volHistory[i]*3*tempS, 4*tempS);
