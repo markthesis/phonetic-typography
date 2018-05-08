0e3e28c25005ef488a38ad3ad7e14b374d161206
markthesis <36646917+markthesis@users.noreply.github.com>
2018-05-08 16:32:46 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -221,7 +221,7 @@ function keyPressed() {
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight - 10);
+  resizeCanvas(windowWidth, windowHeight - 1);
 }
 
 function avgArrayValues(receivedArray, tempX, label) {
